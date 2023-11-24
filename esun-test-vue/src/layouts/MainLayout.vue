8fb0a06ee4581454fcbd8173ac3d860d074f4bd1
Legbro0811 <a126270095@gmail.com>
2023-11-25 04:07:36 +0800

    add MainLayout

---
+++ esun-test-vue/src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="outer">
+    <header>
+      <h1>ESUN TEST - 員工座位系統</h1>
+    </header>
+    <slot></slot>
+  </div>
+</template>
+<style scoped lang="scss">
+.outer {
+  min-height: 100svh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+}
+header {
+  display: flex;
+  background: linear-gradient(to left, white, aquamarine);
+  h1 {
+    padding: 1rem;
+    font-size: x-large;
+    font-weight: bold;
+  }
+}
+</style>
--- esun-test-vue/src/views/HomeView.vue
+++ esun-test-vue/src/views/HomeView.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import MainLayout from '@/layouts/MainLayout.vue'
+</script>
 
 <template>
-  <main></main>
+  <MainLayout>gg</MainLayout>
 </template>
